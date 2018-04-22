af4a68e27b7b7e04534ec2b53ce6e1b722363348
Molan Ren <ren.molan@gmail.com>
2018-04-22 15:01:05 -0400

    Added a time series for artwork acquisition dates

--- MoMA-data-analysis.py
+++ MoMA-data-analysis.py
@@ -1,17 +1,23 @@
-# Import libraries and artwork data
+# Import libraries and data
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 artworks = pd.read_csv('data/artworks.csv')
 print(artworks.shape)
 print(artworks.head())
 
-# Count the number of pieces each artist has on display and show the top 10 artists
-df_artworks = pd.DataFrame(artworks)
-print(df_artworks['Name'].value_counts().head(10))
+artists = pd.read_csv('data/artists.csv')
+print(artists.shape)
+print(artists.head())
 
-# Use matplotlib to create a bar graph to display the 10 artists with the most pieces on display 
-import matplotlib.pyplot as plt
+
+# 1. Artists with most pieces on display
+# Count the number of pieces each artist has on display and show the top 20 artists
+df_artworks = pd.DataFrame(artworks)
+print(df_artworks['Name'].value_counts().head(20))
+
+# Use matplotlib to create a bar graph to display the 20 artists with the most pieces on display 
 top_10_artists = df_artworks['Name'].value_counts()[:20]
 top_10_artists.plot(kind='barh').invert_yaxis()
 plt.ylabel('Artist') 
@@ -19,34 +25,31 @@ plt.xlabel('Number of Pieces on Display')
 plt.suptitle('Artists With the Most Pieces on Display at the MoMA', fontsize=14, fontweight='bold')
 plt.show()
 
-# Import artist data
-artists = pd.read_csv('data/artists.csv')
-print(artists.shape)
-print(artists.head())
 
+# 2. Proportion of male to female artists with pieces on display 
 # Find the proportion of male to female artists who have works on display at the MoMA
 df_artists = pd.DataFrame(artists)
 print(df_artists['Gender'].value_counts())
 
 # Clean up missing values and capitalization inconsistencies in the artist data
-df_artists['Gender'].replace('male', 'Male',inplace=True)
+df_artists['Gender'].replace('male', 'Male', inplace=True)
 df_artists['Gender'].replace(np.nan, 'Unknown', inplace=True)
-print(df_artists['Gender'].value_counts())  
 
 # Check that all artists have been accounted for
 print(sum(df_artists['Gender'].value_counts()) == artists.shape[0])
 
 # Graph the proportion of male to female artists on display
-labels = 'Male', 'Female', 'Unknown'
-sizes = [9826, 3072, 2193]
+labels = df_artists['Gender'].value_counts().index
+sizes = df_artists['Gender'].value_counts().values
 colors = ['gold', 'lightcoral', 'lightskyblue']
 plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=False, startangle=140)
 plt.axis('equal')
 plt.suptitle('Artists with Pieces on Display at the MoMA', fontsize=14, fontweight='bold')
 plt.show()
 
+
+# 3. Artists by nationality 
 # Find the proportion of artists of each nationality 
-print(df_artists['Nationality'].value_counts())
 most_common_nationalities = df_artists['Nationality'].value_counts()[:20]
 most_common_nationalities.plot(kind='barh').invert_yaxis()
 plt.ylabel('Nationality') 
@@ -85,17 +88,30 @@ def classify_nationality(row_index) :
             
 for index, row in df_artists.iterrows() : 
     classify_nationality(index)
-    
-print(df_artists['Continent'].value_counts())
 
-# Graph the proportion of continents represented by the 20 most common nationalities 
-labels = 'North America', 'Europe', 'Asia', 'South America'
-sizes = [5522, 4597, 498, 294]
+# Graph the proportion of continents represented by the 20 most common nationalities of the artists at the MoMA
+labels = df_artists['Continent'].value_counts().index
+sizes = df_artists['Continent'].value_counts().values
 colors = ['gold', 'lightcoral', 'lightskyblue', 'red', 'blue']
 plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=False, startangle=140)
 plt.axis('equal')
-plt.suptitle('Top 20 Most Common Nationalities by Continent', fontsize=14, fontweight='bold')
+plt.suptitle('Top 20 Most Common Artist Nationalities by Continent', fontsize=14, fontweight='bold')
 plt.show()
+
+
+# 4. When were the pieces on display added to the collection at MoMA?
+# Sort the artworks data by 'Acquisition Date'
+df_artworks['Acquisition Date'] = pd.to_datetime(df_artworks['Acquisition Date'], errors='coerce')
+acquisitions = df_artworks['Acquisition Date'].value_counts()
+acquisitions_sorted = acquisitions.sort_index()
+
+# Show the data as a time series
+acquisitions_sorted_cumsum = acquisitions_sorted.cumsum()
+ts = acquisitions_sorted_cumsum.plot(title='Total Number of Pieces Acquired by the MoMA Over Time')
+ts.set_xlabel("Year")
+ts.set_ylabel("Total Number of Artworks Acquired")
+
+
             
 
 
